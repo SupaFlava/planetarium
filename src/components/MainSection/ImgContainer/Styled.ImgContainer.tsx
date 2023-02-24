d38d9fbb40c1e51012125271a67307e95be4e99e
Rami Mhazres <superflava8@gmail.com>
2023-02-24 19:34:14 +0100

    img fix

--- src/components/MainSection/ImgContainer/Styled.ImgContainer.tsx
+++ src/components/MainSection/ImgContainer/Styled.ImgContainer.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const ImgContainer = styled.div`
-  border: none;
-  /* max-width: 100vw; */
   height: 304px;
   display: flex;
+
   justify-content: center;
   align-items: center;
 
   @media (min-width: 768px) {
+    flex-direction: column;
     height: 460px;
   }
   @media (min-width: 1440px) {
--- src/components/MainSection/img/Styled.CoverImg.tsx
+++ src/components/MainSection/img/Styled.CoverImg.tsx
@@ -4,15 +4,13 @@ export const CoverImg = styled.img.attrs(({ src }) => ({
   src: src,
   alt: "Article Cover",
 }))`
-  max-width: 51%;
-  max-height: 51%;
-
+  max-height: 60%;
   @media (min-width: 768px) {
     /* max-width: 40%; */
-    width: auto;
+    max-height: 80%;
   }
 
   @media (min-width: 1440px) {
-    max-width: 100%;
+    max-height: 100%;
   }
 `;
